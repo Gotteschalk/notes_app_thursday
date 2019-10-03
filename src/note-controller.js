37829885d1f66f5f332a329c4e6c3da281578f2b
etelish <etelisha@googlemail.com>
2019-10-03 15:03:30 +0100

    show note in index.html from controller with no test

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,7 +1,21 @@
 (function(exports) {
-   function controllerTest(){
-      document.getElementById('app').innerHTML = "Howdy"
+  function controllerTest(){
+    document.getElementById('app').innerHTML = "Howdy"
   }
-exports.controllerTest = controllerTest
+  exports.controllerTest = controllerTest
 
-})(this)
+
+  function NoteController(noteList){
+      this.noteList = noteList;
+      this.noteList.add("Favourite drink: Seltzer");
+    this.noteListView = new NoteListView(noteList)
+  }
+
+  NoteController.prototype.insertHtmlFromController = function(){    
+    document.getElementById('app').innerHTML = this.noteListView.html();
+  }
+  exports.NoteController = NoteController;
+
+
+
+})(this);
